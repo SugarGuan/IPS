9513727743e082c745dfb3a64a8ab0be178cacc2
关运通 <yuntong.guan@gmail.com>
2019-06-23 12:02:40 +0800

    Fix issues: May some error can be catch in after steps.

--- util/file/Reader.py
+++ util/file/Reader.py
@@ -11,6 +11,7 @@ def read(file_path):
         SystemMessenger.error(2000103)
     if not FileChecker.file_readable(file_path):
         SystemMessenger.error(2000104)
+    file_content = []
     try:
         file = open(file_path, 'r', encoding='utf-8-sig')
         file_content = file.readlines()
@@ -19,11 +20,10 @@ def read(file_path):
         SystemMessenger.error(2000105)
     except IOError as ioe:
         SystemMessenger.error(2000106)
-    except FileNotFoundError as fnfe:
-        SystemMessenger.error(2000107)
-    except FileExistsError as fee:
-        SystemMessenger.error(2000108)
-
+    # except FileNotFoundError as fnfe:
+    #     SystemMessenger.error(2000107)
+    # except FileExistsError as fee:
+    #     SystemMessenger.error(2000108)
     return file_content
 
 
